6274f02f8c474c0be149981f4e48ab13f885ebe2
farjana islam <nipa.farzana87@gmail.com>
2022-07-23 17:53:15 -0700

    fixed add employee form

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
@@ -15,4 +16,45 @@ class EmployeeController extends Controller
     {
         return view('employee.add-employee');
     }
+
+    public function storeEmployee(Request $request)
+    {
+        $request->validate([
+            'name'          => 'required',
+            'img'           => 'nullable',
+            'fathers_name'  => 'required',
+            'nid'           => 'nullable',
+            'gender'        => 'nullable',
+            'address'       => 'required',
+            'joining_date'  => 'required',
+            'position'      => 'required',
+            'salary'        => 'required'
+        ]);
+
+
+
+        $employee = new Employee();
+
+        $employee->name             = $request->name;
+        $employee->fathers_name     = $request->fathers_name;
+        $employee->nid              = $request->nid;
+        $employee->gender           = $request->gender;
+        $employee->address          = $request->address;
+        $employee->joining_date     = $request->joining_date;
+        $employee->position         = $request->position;
+        $employee->salary           = $request->salary;
+        // if($request->hasFile('img')){
+        //     $request->validate([
+        //         'image' => 'mimes:jpeg,bmp,png'
+        //     ]);
+        //     $request->img->store('employee', 'public');
+        //     $employee->img = $request->img->hashName();
+        // }
+
+         $employee->save();
+
+         dd($employee);
+    }
+
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,11 @@ Route::middleware(['auth'])->name('admin.')->prefix('admin')->group(function() {
     Route::get(uri:'/user/{user}/role', action:[UsersController::class, 'assignRoleView'])->name('users.assign.role.view');
     Route::post(uri:'/user/{user}/assign-role', action:[UsersController::class, 'assignRole'])->name('users.roles.assign');
     Route::delete(uri:'/user/{user}/remove-role/{role}', action:[UsersController::class, 'removeRole'])->name('users.roles.remove');
-    Route::get(uri:'/employee', action:[EmployeeController::class, 'allEmployee'])->name('all.employee.show');
 
     // EMPLOYEE ROUTES
+    Route::get(uri:'/employee', action:[EmployeeController::class, 'allEmployee'])->name('all.employee.show');
     Route::get(uri:'/employee/add', action:[EmployeeController::class, 'addEmployee'])->name('add.employee');
+    Route::get(uri:'/employee/store', action:[EmployeeController::class, 'storeEmployee'])->name('store.employee');
 });
 // Route::get('/admin', function () {
 //     return view('admin.index');
