b2690c68be0d92b74cd86aea68351d5c858c46c1
farjana islam <nipa.farzana87@gmail.com>
2022-08-08 11:33:00 +0600

    added some file folder

---
+++ app/Http/Controllers/FabricsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fabrics;
+use Illuminate\Http\Request;
+
+class FabricsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Fabrics  $fabrics
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Fabrics $fabrics)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Fabrics  $fabrics
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Fabrics $fabrics)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Fabrics  $fabrics
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Fabrics $fabrics)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Fabrics  $fabrics
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Fabrics $fabrics)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/IronsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Iron;
+use Illuminate\Http\Request;
+
+class IronsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Iron  $iron
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Iron $iron)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Iron  $iron
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Iron $iron)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Iron  $iron
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Iron $iron)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Iron  $iron
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Iron $iron)
+    {
+        //
+    }
+}
